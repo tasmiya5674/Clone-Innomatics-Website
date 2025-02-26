19da03b3e10315f8b58393152fcb96e4503130c8
tasmiya5674 <tasmiyaattar1510@gmail.com>
2025-02-26 09:23:54 -0800

    Create clone.js

---
+++ clone.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".btn");
+    
+    btn.addEventListener("click", function() {
+        alert("Exploring Courses!");
+    });
+});
